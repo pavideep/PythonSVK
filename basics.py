c565e54b2c901e81b4badfa111c69729a4e4e96c
pavideep <120414424+pavideep@users.noreply.github.com>
2022-12-15 14:16:11 -0600

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,67 @@
+# print("Hello World!")
+
+"""
+This will print 2
+and the next line would print3
+and the previous line must have printed Hello World
+"""
+# print('2')
+# print('3')
+
+# Data types
+
+# Strings, integers, float and complex
+
+# print(type("I am string"))
+# print(type('13'))
+# print(type(10))
+# # 2
+# # -3
+# print(type(-3.2))
+# # 5.20
+# print(type(1j))
+#
+# Mathematical operations
+
+# print(1+2)
+# print(2-1)
+# print(2*3)
+# print(3/2)
+# print(3//2)
+# print(2**3)
+# print(5%2)
+#
+# print(4**(1/2))
+#
+
+
+#string concatenation
+
+# print('Hello ' + 'World')
+# print('Hello', 'World')
+
+# print(2==2)
+# print(3!=2)
+
+## Variables
+
+# x = 'Hello'
+# print(x)
+# # x = 10
+# y = '2'
+# print(type(y))
+#
+# # print(x * y)
+# # print(y * x)
+# #
+# # print(x, y)
+#
+# # print(x + int(y))
+#
+# print(int(y))
+
+
+x = input('Please enter value 1: ')  # string
+y = input('Please enter value 2: ')  # String
+print(int(x) + int(y))
+print(int(x) - int(y))
---
+++ basics_exercises.py
@@ -0,0 +1,68 @@
+
+####### Syntax ##############
+  # print('Hello')
+
+
+######### Variables ##################
+
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# y = 'Apple'
+# print(z)
+# print(y)
+# z = 'Plum'
+
+### Concatnetaion Strings ####
+#
+# print("Hello" + "World")
+#
+# print("Hello", "World")
+#
+# print("Hello " * 5)
+#
+# hello = "Hello"
+# world = "World"
+#
+# print(hello + world)
+#
+# a = 10
+# b = 5
+#
+# print(a - b)
+#
+# # print("14" + 2)
+#
+# print(int("14") + 2)
+# print("14" + str(2))
+
+
+#### type conversion ###
+
+# print(int(3.14))
+#
+# print(int(3.9999))  # This does not round to the closest int!
+#
+# print(int(3.0))
+#
+# print(int(-3.999))  # Note that the result is closer to zero
+
+# minutes = 123435
+# print(int(minutes / 60))
+#
+# int("2345")  # Parse a string to produce an int
+#
+# int(17)  # It even works if arg is already an int
+#
+# print(int("23 bottles"))
+
+
+############## User Input #################
+
+
+# num1 = input("Enter num1: ")
+# num2 = int(input("Enter num2: "))
+#
+# num3 = num1 * num2
+# print("Product is: ", num3)
